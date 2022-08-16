d8b6d70511a4e33966674a32ff581cfee56a8d79
Toby Fleming <sourcecode@tobywf.com>
2022-08-15 19:27:10 -0700

    Add support for reading Readers directly as JSON

---
+++ Mech3DotNet/ReadersJson.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Mech3DotNet.Reader;
+using Mech3DotNet.Unsafe;
+
+namespace Mech3DotNet
+{
+    public static class ReadersJson
+    {
+        private static Dictionary<string, byte[]> Read(string inputPath, bool isPM, out byte[] manifest)
+        {
+            var readers = new Dictionary<string, byte[]>();
+            manifest = Helpers.ReadArchiveRaw(inputPath, isPM, Interop.ReadReader, (string name, byte[] data) =>
+            {
+                readers.Add(name, data);
+            });
+            return readers;
+        }
+
+        public static Dictionary<string, byte[]> ReadMW(string inputPath)
+        {
+            return Read(inputPath, false, out _);
+        }
+
+        public static Dictionary<string, byte[]> ReadPM(string inputPath)
+        {
+            return Read(inputPath, true, out _);
+        }
+    }
+}
